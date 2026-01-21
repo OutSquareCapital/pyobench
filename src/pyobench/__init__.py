52a1b748fa831a70b3b92e8958884eeefa3d88b5
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-01-21 13:23:14 +0100

    exposing Data in public

--- src/pyobench/__init__.py
+++ src/pyobench/__init__.py
@@ -1,5 +1,6 @@
 """Code for benchmarks package."""
 
+from ._pipeline import Data
 from ._registery import REGISTERY, bench
 
-__all__ = ["REGISTERY", "bench"]
+__all__ = ["REGISTERY", "Data", "bench"]
